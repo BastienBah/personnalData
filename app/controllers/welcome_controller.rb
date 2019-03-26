5b87127217004d3985ab0518fe295237855bfc57
BastienBah <bastien.bahuet0704@gmail.com>
2019-03-26 16:39:35 +0100

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,18 @@
+class WelcomeController < ApplicationController
+
+    def index
+        
+    end
+
+    def more
+        
+    end
+
+    def lose
+        
+    end
+
+    def win
+        
+    end
+end
